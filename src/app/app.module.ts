4fb4472f67ba210753f4577ecc864dd70649a719
Farid khan <Farid18khan@gmail.com>
2019-06-24 22:11:01 +0530

    Create Root Commponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  //all the components of modules will be registered here
   declarations: [
     AppComponent
   ],
+  //eternal modules that we what the registered comps to have
   imports: [
     BrowserModule
   ],
   providers: [],
+  //which module will be  bootstrapped
   bootstrap: [AppComponent]
 })
 export class AppModule { }
