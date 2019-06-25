fd481e1da954ec22b6fdaefd528267ad780a8ea2
Farid khan <Farid18khan@gmail.com>
2019-06-25 20:22:44 +0530

    adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductLisComponent } from './products/product-list.component'
 
 @NgModule({
   //all the components of modules will be registered here
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductLisComponent
   ],
   //eternal modules that we what the registered comps to have
   imports: [
